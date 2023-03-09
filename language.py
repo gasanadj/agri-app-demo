770b5e89614e5c89a0a20711fdb3ebd7f53062a3
grace2560 <g.nalule@alustudent.com>
2023-03-09 20:56:19 +0200

    fix language

--- language.py
+++ language.py
@@ -2,8 +2,8 @@ def language():
  print("Choose language")
  print("1.english")
  print("2.kiswahili")
- lang=int(input("Select language (1 or 2: )"))
+ lang=int(input("Select the language  (1 or 2: )"))
  if (lang == 1):
      print("You selected english")
  elif (lang == 2):
-     print("umechagua kiswahiliii") 
+     print("umechagua kiswahili") 
